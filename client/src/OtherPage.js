49171a54bd340388af4943764fb1291b5bb251e2
BezqyczekPL <bezqyczekpl@Dawid.localdomain>
2023-01-27 16:17:13 +0100

    Poprawka nr 2

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      <div> Nazwa programu: MultiFab </div>
+      <div> Nazwa programu: MultiFib </div>
       <div> Dawid Nicpoń </div>
       <div> Grupa 2.3 </div>
       <Link to="/">Wróć do pierwszej strony</Link>
